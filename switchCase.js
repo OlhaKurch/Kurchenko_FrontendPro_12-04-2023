a8c5cc5f9d772186c9904b9a4341ac6cae18e5af
Olha <olga.kurch17@gmail.com>
2023-04-15 13:32:13 +0300

    add dz7 switchase

---
+++ switchCase.js
@@ -0,0 +1,29 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (numOrStr) {
+    case null:
+        console.log('ви скасували');
+        break;
+    case '':
+        console.log('Empty String');
+        break;
+    case !isNaN(numOrStr) || numOrStr: // ідею знайшла в інтернеті, тому що сама не знала як можна порівнювати NaN, якщо він не рівний собі
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+}
+
+
+
+
+// if(numOrStr === null) {
+// console.log('ви скасували')
+// } else if( numOrStr.trim() === '' ) {
+// console.log('Empty String');
+// } else if ( isNaN( +numOrStr ) ) {
+// console.log(' number is Ba_NaN')
+// } else {
+// console.log('OK!')
+// }
